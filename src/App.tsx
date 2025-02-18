36e9f611d849d0d25d77e1810e6516022661c2df
Tony Heiser <tonyhzer@gmail.com>
2025-02-19 00:31:13 +0100

    190225

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.scss'
+import TwoOfThree2 from './components/TwoOfThree2/TwoOfThree2'
 // import Tabs from './components/Tabs/Tabs'
-import TwoOfThree from './components/TwoOfThree/TwoOfThree'
+// import TwoOfThree from './components/TwoOfThree/TwoOfThree'
 
 function App() {
 
   return (
     <>
       {/* <Tabs /> */}
-      <TwoOfThree />
+      {/* <TwoOfThree /> */}
+      <TwoOfThree2 />
     </>
   )
 }
